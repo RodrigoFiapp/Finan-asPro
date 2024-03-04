815614ea41e05e0d787f46c13869517a72195820
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-03-04 19:11:33 -0300

    criação do projeto spring

---
+++ src/main/java/br/com/fiap/financaspro/FinancasproApplication.java
@@ -0,0 +1,23 @@
+package br.com.fiap.financaspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller //requisições de web serão atendidas por ele
+public class FinancasproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasproApplication.class, args);
+	}
+
+	@RequestMapping //requisição para que esse método seja executado
+	@ResponseBody
+	public String home() {
+		return "Finanças Pro";
+	}
+
+}
